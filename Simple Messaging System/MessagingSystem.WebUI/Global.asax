051b423501f631b48b48e12dcb21fbe2a1d47360
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2020-05-20 22:09:28 +0300

    Initial Solution Setup

---
+++ Simple Messaging System/MessagingSystem.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MessagingSystem.WebUI.MvcApplication" Language="C#" %>
---
+++ Simple Messaging System/MessagingSystem.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessagingSystem.WebUI.Startup))]
+namespace MessagingSystem.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
